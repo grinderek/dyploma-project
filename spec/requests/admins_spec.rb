cc5fc68288c2c8ffafb97e99d956fe6e6650b8f4
Danila Berazhny <dannmmaster@gmail.com>
2021-12-22 22:42:35 +0300

    Setup rspec and create tests for user and admin controller

---
+++ spec/requests/admins_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe AdminController, type: :request do
+  it 'renders the index template successfully' do
+    get '/admin'
+    expect(response.body).to include 'This is Admin page!'
+    expect(response).to have_http_status(200)
+  end
+end
---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :request do
+  it "renders the index template successfully" do
+    get '/'
+    expect(response.body).to include 'This is User page!'
+    expect(response).to have_http_status(200)
+  end
+end
