ee8117dfa214b3f4f3dcc52e92529004ead98e6e
Danila Berazhny <dannmmaster@gmail.com>
2021-12-27 17:22:26 +0300

    Create Product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  has_one_attached :image
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 500 }
+  validates :image, blob: { content_type: %w[image/png image/jpg image/jpeg], size_range: 1..(100.megabytes) }
+  validates :code, presence: true, length: { minimum: 7, maximum: 7 }, with: /^[A-Za-z0-9]+$/
+  validates :price, presence: true, numericality: { in: 1..9999999 }
+  validates :description, length: { minimum: 1, maximum: 5000 }
 end
