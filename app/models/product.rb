48edec657ffd7c5ab51495188248e4d8662b4b60
Danila Berazhny <dannmmaster@gmail.com>
2021-12-28 01:04:20 +0300

    Create views for create and edit products and partial for these forms

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ApplicationRecord
 
   validates :name, presence: true, length: { minimum: 2, maximum: 500 }
   validates :image, blob: { content_type: %w[image/png image/jpg image/jpeg], size_range: 1..(100.megabytes) }
-  validates :code, presence: true, length: { minimum: 7, maximum: 7 }, with: /^[A-Za-z0-9]+$/
+  validates :code, presence: true, length: { minimum: 7, maximum: 7 }, format: { with: /\A[A-Za-z0-9]+\z/ }
   validates :price, presence: true, numericality: { in: 1..9999999 }
   validates :description, length: { minimum: 1, maximum: 5000 }
 end
