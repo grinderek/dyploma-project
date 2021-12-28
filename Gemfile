5fe8bfdff308f47ae20076c79aa3050b28de73f4
Danila Berazhny <dannmmaster@gmail.com>
2021-12-28 17:51:11 +0300

    Create all error messages

--- Gemfile
+++ Gemfile
@@ -22,7 +22,7 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem 'haml'
-gem 'activestorage-validator'
+gem 'active_storage_validations'
 gem "bootstrap_form", "~> 5.0"
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,19 @@
 class Product < ApplicationRecord
   has_one_attached :image
 
-  validates :name, presence: true, length: { minimum: 2, maximum: 500 }
-  validates :image, blob: { content_type: %w[image/png image/jpg image/jpeg], size_range: 1..(100.megabytes) }
-  validates :code, presence: true, length: { minimum: 7, maximum: 7 }, format: { with: /\A[A-Za-z0-9]+\z/, message: "Only alphanumeric characters are allowed" }
-  validates :price, presence: true, numericality: { in: 1..9999999 }
-  validates :description, length: { maximum: 5000 }
+  validates :image, content_type: { in: %w[image/png image/jpg image/jpeg], message: "This format is not allowed. Allowed formats are jpg, jpeg, png" },
+                    size: { less_than: 100.megabytes , message: "File size is too big. Max allowed size is 100 Mb." }
+
+  validates_presence_of :name, message: "This field is required"
+  validates_length_of :name, minimum: 2, maximum: 500, message: "Min characters for this field is 2. Max characters for this field is 500"
+
+  validates_presence_of :code, message: "This field is required"
+  validates_length_of :code, minimum: 7, maximum: 7, message: "Min characters for this field is 7. Max characters for this field is 7"
+  validates_format_of :code, with: /\A[A-Za-z0-9]+\z/, message: "Only alphanumeric characters are allowed"
+
+  validates_presence_of :price, message: "This field is required"
+  validates_numericality_of :price, message: "Only numbers are allowed"
+  validates_length_of :price, minimum: 1, maximum: 7, message: "Min characters for this field is 1. Max characters for this field is 7"
+
+  validates_length_of :description, maximum: 5000, message: "Min characters for this field is 1. Max characters for this field is 5000"
 end
