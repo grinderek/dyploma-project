60dcd8c37451125f23126d0319cbb57928404c00
Danila Berazhny <dannmmaster@gmail.com>
2021-12-28 19:43:26 +0300

    Fix edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,7 +5,7 @@ class ProductsController < ApplicationController
   end
 
   def edit
-
+    @product = Product.find(params[:id])
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   scope '/admin' do
     get '/', to: 'welcome#index'
     resources :products
+    resource :product
   end
 end
