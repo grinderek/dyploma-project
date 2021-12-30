e0468170668e00c38f6c00d4d36478bd4b394642
Danila Berazhny <dannmmaster@gmail.com>
2021-12-30 14:08:10 +0300

    Add arrow js functions

--- app/javascript/packs/delete_button.js
+++ app/javascript/packs/delete_button.js
@@ -1,18 +1,4 @@
-$('#input').change(function () {
-    button()
-})
-
-$('#button').click(function () {
-    $('#input').val('')
-    let elem = document.getElementById('your_preview_id')
-    while(elem.attributes.length > 1)
-        elem.removeAttribute(elem.attributes[1].name);
-    button()
-})
-
-button()
-
-function button() {
+let button = () => {
     let elem = document.getElementById('your_preview_id')
     if(elem == null || ($('#input').get(0).files.length === 0 && elem.attributes.length === 1)){
         $('#button').hide();
@@ -21,3 +7,15 @@ function button() {
         $('#button').show();
     }
 }
+
+$('#input').change(() => button())
+
+$('#button').click(() => {
+    $('#input').val('')
+    let elem = document.getElementById('your_preview_id')
+    while(elem.attributes.length > 1)
+        elem.removeAttribute(elem.attributes[1].name);
+    button()
+})
+
+button()
--- app/javascript/packs/show_image.js
+++ app/javascript/packs/show_image.js
@@ -1,18 +1,14 @@
-$('#input').on('change', function() {
-    showImage(this);
-})
-
-function showImage(input) {
+let showImage = input => {
     if (input.files && input.files[0]) {
         let reader = new FileReader();
 
-        reader.onload = function (e) {
-            $('#your_preview_id')
-                .attr('src', e.target.result)
-                .width(500)
-                .height(200);
-        };
+        reader.onload = e => $('#your_preview_id')
+            .attr('src', e.target.result)
+            .width(500)
+            .height(200);
 
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+$('#input').change(function () {showImage(this)})
