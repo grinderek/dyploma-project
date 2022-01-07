940a8b464aa5dd2556f6bf6373149c7e566b967f
Danila Berazhny <dannmmaster@gmail.com>
2022-01-07 11:36:22 +0300

    Fix for circle CI

--- spec/features/list_of_products_spec.rb
+++ spec/features/list_of_products_spec.rb
@@ -8,24 +8,19 @@ RSpec.feature 'List of products', type: :feature do
       @product = build(:product)
       @product.save
     end
-    Capybara.current_driver = :selenium_chrome
     visit products_path
-  end
-
-  after(:all) do
-    Capybara.use_default_driver
   end
 
   scenario 'Delete button should be disabled when all products unchecked' do
     expect(page).to have_button('Delete Products', disabled: true)
   end
 
-  scenario 'Delete button should be active when at least one product checked' do
+  scenario 'Delete button should be active when at least one product checked', :js do
     find(:css, '#check_1').set(true)
     expect(page).to have_button('Delete Products', disabled: false)
   end
 
-  scenario 'Delete products when click on Delete Products button' do
+  scenario 'Delete products when click on Delete Products button', :js do
     find(:css, '#check_1').set(true)
     expect do
       click_button 'Delete Products'
@@ -34,7 +29,7 @@ RSpec.feature 'List of products', type: :feature do
     end.to change { Product.count }.by(-1)
   end
 
-  scenario 'Select check_all checkbox and click Delete button' do
+  scenario 'Select check_all checkbox and click Delete button', :js do
     find(:css, '#check_all').set(true)
     expect do
       click_button 'Delete Products'
