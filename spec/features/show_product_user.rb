f636031af95c19d2b9f6a4f5deb1abeabc7e15c0
Danila Berazhny <dannmmaster@gmail.com>
2022-01-11 14:02:32 +0300

    DP2-7 - Add tests

---
+++ spec/features/show_product_user.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.feature 'Show product[User]', type: :feature do
+  before(:each) do
+    build_list(:product, 11, &:save)
+    visit user_product_index_path
+  end
+
+  scenario 'Number of products should be hide when equal to 0' do
+    expect(page).to_not have_selector('#count_of_products')
+  end
+
+  scenario 'After add product show count of products' do
+    find('#add_1').click
+    expect(page).to have_selector('#count_of_products')
+  end
+
+  scenario 'Go to different page' do
+    click_link('2', exact: true)
+    expect(page).to have_selector('#product_11')
+  end
+end
