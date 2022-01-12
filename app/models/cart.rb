11b772f9231fe8a930353f1a52ef05230e330a7c
Danila Berazhny <dannmmaster@gmail.com>
2022-01-13 00:41:09 +0300

    DP2-8 - Add logic in models

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,4 +2,15 @@
 
 class Cart < ApplicationRecord
   has_many :cart_items
+  before_save :set_subtotal
+
+  def subtotal
+    cart_items.sum { |cart_item| cart_item.valid? ? cart_item.unit_price * cart_item.quantity : 0 }
+  end
+
+  private
+
+  def set_subtotal
+    self[:subtotal] = subtotal
+  end
 end
