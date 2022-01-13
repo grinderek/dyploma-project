b267a44160eeeab0c9f424b4deda9cf107ba013b
Danila Berazhny <dannmmaster@gmail.com>
2022-01-13 15:44:31 +0300

    DP2-6 - Undo product model changes

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@
 
 class Product < ApplicationRecord
   has_one_attached :image
+  has_many :cart_items, dependent: :destroy
 
   validates :image, content_type: { in: %w[image/png image/jpg image/jpeg] }, size: { less_than: 100.megabytes }
   validates :name, presence: true, length: { minimum: 2, maximum: 500 }
