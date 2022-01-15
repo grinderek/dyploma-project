c6b3cbd8bed13002f4bc371f10dd3e94a6a6297d
Danila Berazhny <dannmmaster@gmail.com>
2022-01-16 01:47:11 +0300

    DP2-9 - Add validation to checkout model

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -2,4 +2,8 @@
 
 class Checkout < ApplicationRecord
   has_one :cart, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 1, maximum: 20 }
+  validates :email, presence: true, format: { with: /\A[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\z/ }
+  validates :address, length: { minimum: 20 }
 end
