5b16b045cab9cf08eba4ef141b6efebf6481f525
Danila Berazhny <dannmmaster@gmail.com>
2022-01-15 14:08:27 +0300

    DP2-8 - Add tests

--- spec/features/cart_spec.rb
+++ spec/features/cart_spec.rb
@@ -6,12 +6,24 @@ RSpec.feature 'Show product[User]', type: :feature do
   before(:each) do
     create_list(:product, 11)
     @product = Product.last
+  end
+
+  scenario 'Visit empty cart' do
+    visit cart_path
+    expect(page).to have_content('The cart is empty')
+  end
+
+  scenario 'Show product when cart not empty' do
     visit user_product_path(@product)
     find('input[name="commit"]').click
     visit cart_path
+    expect(page).to have_selector('#product_11')
   end
 
   scenario 'Increase product count' do
+    visit user_product_path(@product)
+    find('input[name="commit"]').click
+    visit cart_path
     find('input[name="commit"]').click
     expect(page).to have_selector('#count_11', text: '2')
   end
