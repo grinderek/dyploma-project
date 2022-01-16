79cd96e6c5e5880d12f5d038c2ece7a55db456dd
Danila Berazhny <dannmmaster@gmail.com>
2022-01-16 23:25:42 +0300

    DP2-6 - Fixes

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,7 +4,7 @@ class Cart
   attr_accessor :items
 
   def initialize(params = {})
-    params = {} if params.nil?
+    params = { 'items' => {} } if params.nil?
     @items = []
     params['items'].each do |key, _value|
       cart_item = CartItem.new(key['product_id'], key['quantity'])
