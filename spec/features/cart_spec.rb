da7a2dc63d0d24fd725ca94b0405374362c44df2
Danila Berazhny <dannmmaster@gmail.com>
2022-01-17 17:16:35 +0300

    DP2-8 - Fix tests

--- spec/features/cart_spec.rb
+++ spec/features/cart_spec.rb
@@ -2,7 +2,7 @@
 
 require 'rails_helper'
 
-RSpec.feature 'Show product[User]', type: :feature do
+RSpec.feature 'Cart[User]', type: :feature do
   before(:each) do
     create_list(:product, 11)
     @product = Product.last
@@ -15,16 +15,16 @@ RSpec.feature 'Show product[User]', type: :feature do
 
   scenario 'Show product when cart not empty' do
     visit user_product_path(@product)
-    find('input[name="commit"]').click
+    find('#add_11').click
     visit cart_path
     expect(page).to have_selector('#product_11')
   end
 
   scenario 'Increase product count' do
     visit user_product_path(@product)
-    find('input[name="commit"]').click
+    find('#add_11').click
     visit cart_path
-    find('input[name="commit"]').click
+    find('#add_11').click
     expect(page).to have_selector('#count_11', text: '2')
   end
 end
