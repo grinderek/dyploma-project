3505c4b45bbe91905aa6d2103fdb1abda4ef73a2
Danila Berazhny <dannmmaster@gmail.com>
2022-01-20 13:19:30 +0300

    DP2-9 - Create migration for promocodes

---
+++ app/models/promocode.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class PromoCode < ApplicationRecord
+  has_one_attached :image
+
+  validates :image, content_type: { in: %w[image/png image/jpg image/jpeg] }, size: { less_than: 100.megabytes }
+  validates :name, presence: true, length: { minimum: 2, maximum: 500 }
+  validates :code, presence: true, length: { minimum: 7, maximum: 7 }, format: { with: /\A[A-Za-z0-9]+\z/ }
+  validates :price, presence: true, length: { minimum: 1, maximum: 7 }, numericality: true
+  validates :description, length: { maximum: 5000 }
+end
