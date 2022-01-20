eb4551bf6f62978ebd023d010e94538c2c096a60
Danila Berazhny <dannmmaster@gmail.com>
2022-01-20 13:20:48 +0300

    DP2-9 - Schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_15_205954) do
+ActiveRecord::Schema.define(version: 2022_01_20_101005) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -54,6 +54,13 @@ ActiveRecord::Schema.define(version: 2022_01_15_205954) do
     t.string "code"
     t.integer "price"
     t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "promo_codes", force: :cascade do |t|
+    t.string "code"
+    t.integer "percent"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
