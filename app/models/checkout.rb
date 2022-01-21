f957d2edc6fb31b5957121e584733b5f530ee8df
Danila Berazhny <dannmmaster@gmail.com>
2022-01-21 23:01:13 +0300

    DP2-9 - Add create new promo code functionality

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Checkout < ApplicationRecord
-  has_one :cart, dependent: :destroy
-
   validates :name, presence: true, length: { minimum: 1, maximum: 20 }
   validates :email, presence: true, 'valid_email_2/email': true
   validates :address, presence: true, length: { minimum: 20 }
--- app/models/promo_code.rb
+++ app/models/promo_code.rb
@@ -2,4 +2,10 @@
 
 class PromoCode < ApplicationRecord
 
+  validates :code, presence: true, uniqueness: true
+  validates :percent, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 1, less_than_or_equal_to: 100,
+  }
+
 end
