b8df53e9ea8f7ba76953c005d8c59d68dbf1ccfd
Danila Berazhny <dannmmaster@gmail.com>
2022-01-22 20:40:48 +0300

    DP2-9 - Add total price field to checkout form and change schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_20_101005) do
+ActiveRecord::Schema.define(version: 2022_01_22_173939) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 2022_01_20_101005) do
     t.string "address"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "total"
   end
 
   create_table "products", force: :cascade do |t|
