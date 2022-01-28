7fbcbdb3e5f0eeb7773f441b7b51f2991d1bd9fd
grinderek <dannmmaster@gmail.com>
2022-01-28 14:24:47 +0300

    DP2-14 - Add total to order model and total_item_price to order_product model

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,8 @@ class Order < ApplicationRecord
   validates :customer_name, presence: true, length: { minimum: 1, maximum: 20 }
   validates :email, presence: true, 'valid_email_2/email': true
   validates :delivery_address, presence: true, length: { minimum: 20 }
+
+  def total
+    order_products.inject(0) { |sum, order_product| sum + order_product.total_item_price } * (1 - (discount / 100.0))
+  end
 end
