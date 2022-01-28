0fb58dae3fe5cc52cbe8f9741c5b0d21a1998d06
grinderek <dannmmaster@gmail.com>
2022-01-28 13:45:17 +0300

    DP2-14 - Add deleted field to products and change destroy action

--- app/services/session_cart.rb
+++ app/services/session_cart.rb
@@ -7,7 +7,7 @@ class SessionCart
     params = { 'items' => {} } if params.nil?
     @items = []
     params['items'].each do |key, _value|
-      unless ProductFinder.search(id: key['product_id']).empty?
+      unless ProductFinder.search(id: key['product_id']).first.deleted == true
         cart_item = SessionCartItem.new(key['product_id'], key['quantity'])
         @items.push(cart_item)
       end
